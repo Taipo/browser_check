63cb640ce6a934bef0c4ee08d0e005485403500e
Te Taipo <hokioi-security@protonmail.ch>
2021-12-14 10:00:29 +1300

    Update class_is_browser.php

--- class_is_browser.php
+++ class_is_browser.php
@@ -56,106 +56,106 @@ class isBrowser_Filter {
 			if ( in_array( $momo_aho, hash_algos() ) ) return $momo_aho;
 		}
 	}
-    public static function clear_session() {
-      $getvariables = array_keys( $_SESSION );
-      if ( empty( $getvariables ) ) return;
-      $count = 0;
-      while( $count < count( $getvariables ) ) {
-        if ( substr( $getvariables[ $count ], 0, 12 ) == 'browser-test' ) {
-            unset( $_SESSION[ $getvariables[ $count ] ] );
-        }
-        $count++;
-      }
-      return true;
-    }
-    public static function get_server_sig( $rand = false ) {
-         if ( false !== $rand ) return base64_encode( random_bytes( 24 ) );
-         # aggregate static variables
-         $_SERVERVARS = '';
-         $serverVars  = array(
-        'HTTP_HOST',
-        'SERVER_ADMIN',
-        'HTTP_USER_AGENT',
-        'SERVER_ADDR',
-        'REMOTE_ADDR',
-        'DOCUMENT_ROOT',
-        'HTTP_ACCEPT',
-        'HTTP_X_CLUSTER_CLIENT_IP',
-        'HTTP_X_FORWARDED_FOR',
-        'HTTP_X_ORIGINAL_URL',
-        'HTTP_CONNECTION',
-        'HTTP_X_REWRITE_URL',
-        'HTTP_CLIENT_IP',
-        'HTTP_PROXY_USER',
-        'GATEWAY_INTERFACE',
-        'SERVER_SOFTWARE',
-        'SERVER_PROTOCOL',
-        'HTTP_ACCEPT_ENCODING',
-        'HTTP_ACCEPT_LANGUAGE',
-        'PATH',
-        'SERVER_SIGNATURE',
-        'SERVER_SOFTWARE',
-        'SERVER_NAME',
-        'SERVER_ADDR',
-        'SERVER_PORT',
-         );
-         $x           = 0;
-         while ( $x < count( $serverVars ) ) {
-             if ( false !== isset( $_SERVER[ $serverVars[ $x ] ] ) )
-                 $_SERVERVARS = $_SERVERVARS . $_SERVER[ $serverVars[ $x ] ];
-             $x++;
-         }
-         return self::make_hash( self::HASH_TYPE, preg_replace( "/[\s]/i", '', self::TAUROA . $_SERVERVARS ) );
-    }
-    public static function make_hash( $sType, $input, $withSalt = false ) {
-        $hashtype = self::get_hash_type( $sType );
-        // format: algorithm:iterations:salt:hash
-        if ( false !== $withSalt )
-            $salt = base64_encode( random_bytes( 24 ) );
-        return hash( $hashtype, hash_pbkdf2( $hashtype, $input, ( ( false !== $withSalt ) ? $salt : NULL ), 4096, 96 ) );
-    }
-    public static function get_hash_type( $sType ) {
-        foreach ( hash_algos() as $key => $val ) {
-            if ( $key == $sType ) {
-                return $val;
-            }
-        }
-        return;
-    }     
-    public static function get_time_hash() {
-        $time_array   = explode( ' ', gmdate( 'D, d M Y H:i:s', time() + ( 12 * 3600 ) ) );
-        $dlist        = array(
-                               "Monday",
-                               "Tuesday",
-                               "Wednesday",
-                               "Thursday",
-                               "Friday",
-                               "Saturday",
-                               "Sunday" 
-                               );
-        $mlist        = array(
-                               "January",
-                               "February",
-                               "March",
-                               "April",
-                               "May",
-                               "June",
-                               "July",
-                               "August",
-                               "September",
-                               "October",
-                               "November",
-                               "December" 
-                               );
-        $fullmonth    = static::make_hash( self::HASH_TYPE, $mlist[ ( ( int ) gmdate( "m" ) ) - 1 ] . self::SESS_TIME );
-        for ( $x = 0; $x < count( $dlist ); $x++ ) {
-            if ( false !== strpos( $dlist[ $x ], gmdate( "D" ) ) ) {
-                $fullday = static::make_hash( self::HASH_TYPE, $dlist[ $x ] );
-                break;
-            }
-        }
-        $ampm       = static::make_hash( self::HASH_TYPE, ( ( int ) substr( $time_array[ 4 ], 0, 2 ) > 11 ) ? 'PM' . $fullday : 'AM' . $fullmonth );
-        return static::make_hash( self::HASH_TYPE, ( substr( preg_replace( "/[\s]/i", '', gmdate( 'D, d M Y H:i:s', time() + ( 12 * 3600 ) ) ), 0, ( int ) strpos( preg_replace( "/[\s]/i", '', gmdate( 'D, d M Y H:i:s', time() + ( 12 * 3600 ) ) ), ":" ) ) . ( isset( $_SERVER[ 'REQUEST_TIME_FLOAT' ] ) ? substr( $_SERVER[ 'REQUEST_TIME_FLOAT' ], 0, 6 ) : substr( $_SERVER[ 'REQUEST_TIME' ], 0, 7 ) ) ) );        
-    }
+	public static function clear_session() {
+		$getvariables = array_keys( $_SESSION );
+		if ( empty( $getvariables ) ) return;
+		$count = 0;
+		while( $count < count( $getvariables ) ) {
+			if ( substr( $getvariables[ $count ], 0, 12 ) == 'browser-test' ) {
+				unset( $_SESSION[ $getvariables[ $count ] ] );
+			}
+		$count++;
+		}
+		return true;
+	}
+	public static function get_server_sig( $rand = false ) {
+		if ( false !== $rand ) return base64_encode( random_bytes( 24 ) );
+		# aggregate static variables
+		$_SERVERVARS = '';
+		$serverVars  = array(
+		'HTTP_HOST',
+		'SERVER_ADMIN',
+		'HTTP_USER_AGENT',
+		'SERVER_ADDR',
+		'REMOTE_ADDR',
+		'DOCUMENT_ROOT',
+		'HTTP_ACCEPT',
+		'HTTP_X_CLUSTER_CLIENT_IP',
+		'HTTP_X_FORWARDED_FOR',
+		'HTTP_X_ORIGINAL_URL',
+		'HTTP_CONNECTION',
+		'HTTP_X_REWRITE_URL',
+		'HTTP_CLIENT_IP',
+		'HTTP_PROXY_USER',
+		'GATEWAY_INTERFACE',
+		'SERVER_SOFTWARE',
+		'SERVER_PROTOCOL',
+		'HTTP_ACCEPT_ENCODING',
+		'HTTP_ACCEPT_LANGUAGE',
+		'PATH',
+		'SERVER_SIGNATURE',
+		'SERVER_SOFTWARE',
+		'SERVER_NAME',
+		'SERVER_ADDR',
+		'SERVER_PORT',
+		);
+		$x		   = 0;
+		while ( $x < count( $serverVars ) ) {
+			if ( false !== isset( $_SERVER[ $serverVars[ $x ] ] ) )
+				$_SERVERVARS = $_SERVERVARS . $_SERVER[ $serverVars[ $x ] ];
+			$x++;
+		}
+		return self::make_hash( self::HASH_TYPE, preg_replace( "/[\s]/i", '', self::TAUROA . $_SERVERVARS ) );
+	}
+	public static function make_hash( $sType, $input, $withSalt = false ) {
+		$hashtype = self::get_hash_type( $sType );
+		// format: algorithm:iterations:salt:hash
+		if ( false !== $withSalt )
+			$salt = base64_encode( random_bytes( 24 ) );
+		return hash( $hashtype, hash_pbkdf2( $hashtype, $input, ( ( false !== $withSalt ) ? $salt : NULL ), 4096, 96 ) );
+	}
+	public static function get_hash_type( $sType ) {
+		foreach ( hash_algos() as $key => $val ) {
+			if ( $key == $sType ) {
+				return $val;
+			}
+		}
+		return;
+	}	 
+	public static function get_time_hash() {
+		$time_array   = explode( ' ', gmdate( 'D, d M Y H:i:s', time() + ( 12 * 3600 ) ) );
+		$dlist		= array(
+							"Monday",
+							"Tuesday",
+							"Wednesday",
+							"Thursday",
+							"Friday",
+							"Saturday",
+							"Sunday" 
+							 );
+		$mlist		= array(
+							"January",
+							"February",
+							"March",
+							"April",
+							"May",
+							"June",
+							"July",
+							"August",
+							"September",
+							"October",
+							"November",
+							"December" 
+							);
+		$fullmonth	= static::make_hash( self::HASH_TYPE, $mlist[ ( ( int ) gmdate( "m" ) ) - 1 ] . self::SESS_TIME );
+		for ( $x = 0; $x < count( $dlist ); $x++ ) {
+			if ( false !== strpos( $dlist[ $x ], gmdate( "D" ) ) ) {
+				$fullday = static::make_hash( self::HASH_TYPE, $dlist[ $x ] );
+				break;
+			}
+		}
+		$ampm	= static::make_hash( self::HASH_TYPE, ( ( int ) substr( $time_array[ 4 ], 0, 2 ) > 11 ) ? 'PM' . $fullday : 'AM' . $fullmonth );
+		return static::make_hash( self::HASH_TYPE, ( substr( preg_replace( "/[\s]/i", '', gmdate( 'D, d M Y H:i:s', time() + ( 12 * 3600 ) ) ), 0, ( int ) strpos( preg_replace( "/[\s]/i", '', gmdate( 'D, d M Y H:i:s', time() + ( 12 * 3600 ) ) ), ":" ) ) . ( isset( $_SERVER[ 'REQUEST_TIME_FLOAT' ] ) ? substr( $_SERVER[ 'REQUEST_TIME_FLOAT' ], 0, 6 ) : substr( $_SERVER[ 'REQUEST_TIME' ], 0, 7 ) ) ) );		
+	}
 }
 ?>
